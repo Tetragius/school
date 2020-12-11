8f45727f602a780191e1679d81bfaa566103f558
Tetragius <tetragion@yandex.ru>
2020-12-11 20:45:24 +0300

    wip

---
+++ server/static.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname + "/../build")));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../build/index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
