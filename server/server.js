38dd39c892d0f2bc2ff31666c7b11a8ed699423d
Tetragius <tetragion@yandex.ru>
2020-12-12 00:55:37 +0300

    wip

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const bodyParser = require("body-parser");
 var MongoClient = require("mongodb").MongoClient;
 var ObjectId = require("mongodb").ObjectId;
